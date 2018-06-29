40e5b667d690ea6a6dd84ae36c744b02ab35c007
MomenNano <momenslah1998@gmail.com>
2018-06-30 00:49:05 +0200

    add some volley

--- app/src/main/java/com/android/nfc/purchasersmartpay/LoginActivity.java
+++ app/src/main/java/com/android/nfc/purchasersmartpay/LoginActivity.java
@@ -5,52 +5,28 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.android.nfc.purchasersmartpay.R;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
+import com.android.nfc.purchasersmartpay.R;
 
 public class LoginActivity extends Activity {
-    String myurl = "http://ddfeee91.ngrok.io";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+            startActivity(intent);
 
     }
     public void login(View view){
         EditText username = (EditText) findViewById(R.id.login_username);
         EditText password = (EditText) findViewById(R.id.login_password);
-        Button btn = (Button) findViewById(R.id.loginBtn);
 
-        final RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, myurl,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("sucess","sucess");
-                        requestQueue.stop();
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("error","eroor");
-                        requestQueue.stop();
-                    }
-                });
-        requestQueue.add(stringRequest);
+            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+            startActivity(intent);
+
     }
-
 }
